56402ef6e4f83255aab752f78c73e388a24c6c60
fochit00 <73275843+fochit00@users.noreply.github.com>
2024-06-13 07:42:14 +0200

    main

---
+++ src/main/java/com/example/springbootminimalanwendung/SpringBootMinimalAnwendungApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootminimalanwendung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMinimalAnwendungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMinimalAnwendungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootminimalanwendung/SpringBootMinimalAnwendungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootminimalanwendung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMinimalAnwendungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
